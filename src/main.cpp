78835c5f19a8e64ddb8dc872ff2d31d32b3e5096
kok-s0s <2694308562@qq.com>
2023-12-14 12:03:46 +0800

    将 FTXUI 作为 Git 子模块来使用

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "controller/todo_controller.h"
 
-int main(int argc, char *argv[]) {
-  TodoController todoController;
-  todoController.run();
+int main(int argc, char* argv[]) {
+  // TodoController todoController;
+  // todoController.run();
 
   return 0;
 }
